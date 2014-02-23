07121b7797dab3f7214a95653d132f731ea59bb3
Martin Schürrer <martin@schuerrer.org>
2014-02-22 17:11:57 -0800

    Cleanup, add ThreadLocalDelegator.

--- lib/threadlocal.rb
+++ lib/threadlocal.rb
@@ -1,4 +1,5 @@
 require 'threadlocal/version'
+require 'delegate'
 
 class ThreadLocal
   @mutex = Mutex.new
@@ -7,27 +8,69 @@ class ThreadLocal
     @mutex.synchronize { @counter += 1 }
   end
 
-  def self.finalize(id, _object_id)
-    Thread.list.each do |thread|
-      if thread_locals = Thread.current.thread_variable_get(:thread_locals)
-        thread_locals.delete id
+  def self.finalizer_proc(id)
+    proc do |_object_id|
+      Thread.list.each do |thread|
+        if thread_locals = Thread.current.thread_variable_get(:thread_locals)
+          thread_locals.delete id
+        end
       end
     end
   end
 
-  def initialize
+  def initialize(default=nil, &default_proc)
+    raise ArgumentError.new("either supply default or default_proc, not both") if default && default_proc
+    @default = default.dup if default
+    @default_proc = default_proc if default_proc
+
     @id = ThreadLocal.next_id
-    ObjectSpace.define_finalizer(self, self.class.method(:finalize).to_proc.curry(2)[@id])
+    ObjectSpace.define_finalizer(self, ThreadLocal.finalizer_proc(@id))
   end
 
-  def set(value)
-    thread_locals = Thread.current.thread_variable_get(:thread_locals) || Thread.current.thread_variable_set(:thread_locals, {})
-    thread_locals[@id] = value
+  def set(obj)
+    self.locals_hash[@id] = obj
   end
 
   def get
-    if thread_locals = Thread.current.thread_variable_get(:thread_locals)
-      thread_locals[@id]
+    if self.exist?
+      self.locals_hash[@id]
+    elsif @default
+      self.set @default.dup
+    elsif @default_proc
+      self.set @default_proc.call
+    else
+      nil
     end
+  end
+
+  def delete
+    self.locals_hash.delete @id
+  end
+
+  def exist?
+    self.locals_hash.has_key? @id
+  end
+
+  def has_default?
+    @default_value || @default_proc
+  end
+
+  protected
+  def locals_hash
+    Thread.current.thread_variable_get(:thread_locals) || Thread.current.thread_variable_set(:thread_locals, {})
+  end
+end
+
+class ThreadLocalDelegator < Delegator
+  def initialize(default=nil, &default_proc)
+    @threadlocal = ThreadLocal.new(default, &default_proc)
+  end
+
+  def __getobj__
+    @threadlocal.get
+  end
+
+  def __setobj__(obj)
+    @threadlocal.set obj
   end
 end
